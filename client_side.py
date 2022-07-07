63735f2328b9a69eab40925d2a2efd44d3074172
Sergei Fedotov <sergeyfedotov083@gmail.com>
2022-07-07 23:28:27 +0300

    Added put method to server and client sides

---
+++ client_side.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+base = 'http://127.0.0.1:5000/'
+with open('test_case_simple_upload.txt', encoding='utf-8') as f:
+    for batch in f:
+        batch = json.loads(batch)
+        requests.post(base + 'put_sessions', json=batch)
+        
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cassandra.cluster import Cluster
 import json
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 # Connect to Cassandra
 # TODO: provide correct credentials
@@ -28,6 +30,7 @@ CREATE TABLE IF NOT EXISTS {table_name_start_session_events} (
     PRIMARY KEY (country, ts, session_id)
 ) WITH CLUSTERING ORDER BY (ts DESC, session_id ASC);
 """
+session.execute(table_ddl_start_session_events)
 
 # Initialize end_session_events table
 table_name_end_session_events = 'end_session_events'
@@ -52,7 +55,7 @@ insert_query_template_end_session = """
     INSERT INTO {table_name} (player_id, ts, session_id, event)
     VALUES ('{player_id}', '{ts}', '{session_id}', '{event_type}')
 """
-
+"""
 test_case_simple_upload_f = open('test_case_simple_upload.txt', encoding='utf-8')
 for batch_str in test_case_simple_upload_f:
     batch = json.loads(batch_str)
@@ -78,3 +81,39 @@ for batch_str in test_case_simple_upload_f:
             session.execute(insert_query_end_session)
 
 test_case_simple_upload_f.close()
+"""
+# Start rest api server
+app = Flask(__name__)
+api = Api(app)
+
+
+class PutSessions(Resource):
+    def post(self):
+        events = request.get_json()
+        for event in events:
+            if event.get('event') == 'start':
+                insert_query_start_session = insert_query_template_start_session.format(
+                    table_name=table_name_start_session_events,
+                    country=event.get('country'),
+                    player_id=event.get('player_id'),
+                    ts=event.get('ts'),
+                    session_id=event.get('session_id'),
+                    event_type=event.get('event')
+                )
+                session.execute(insert_query_start_session)
+            elif event.get('event') == 'end':
+                insert_query_end_session = insert_query_template_end_session.format(
+                    table_name=table_name_end_session_events,
+                    player_id=event.get('player_id'),
+                    ts=event.get('ts'),
+                    session_id=event.get('session_id'),
+                    event_type=event.get('event')
+                )
+                session.execute(insert_query_end_session)
+        return '', 201
+
+
+api.add_resource(PutSessions, '/put_sessions')
+
+if __name__ == '__main__':
+    app.run(debug=True)
