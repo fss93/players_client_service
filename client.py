77c05a2ee3f4be0426b519d74cc2dfb6fb232feb
Sergei Fedotov <sergeyfedotov083@gmail.com>
2022-07-07 22:54:14 +0300

    Tutorial. Post list to rest api server

--- client.py
+++ client.py
@@ -4,5 +4,5 @@ base = 'http://127.0.0.1:5000/'
 response = requests.get(base + 'helloworld')
 print(response.json())
 
-response_2 = requests.post(base + 'helloworld')
+response_2 = requests.post(base + 'helloworld', json=[{'a': 1}, {'b': 2}])
 print(response_2.json())
--- youtube_tutorial.py
+++ youtube_tutorial.py
@@ -1,8 +1,11 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+sessions_put_args = reqparse.RequestParser()
+sessions_put_args.add_argument('session_list', type=list)
 
 
 class HelloWorld(Resource):
@@ -10,7 +13,9 @@ class HelloWorld(Resource):
         return {'data': 'Hello, World!'}
 
     def post(self):
-        return {'data': 'posted'}
+        sessions = request.get_json()
+        print(sessions, type(sessions))
+        return '', 201
 
 
 api.add_resource(HelloWorld, '/helloworld')
