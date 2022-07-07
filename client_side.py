e046eab9979361cd0a47f6a98a37613c7c9a0e6f
Sergei Fedotov <sergeyfedotov083@gmail.com>
2022-07-08 00:44:18 +0300

    Insert all test cases through rest api client

--- client_side.py
+++ client_side.py
@@ -5,5 +5,5 @@ base = 'http://127.0.0.1:5000/'
 with open('test_case_simple_upload.txt', encoding='utf-8') as f:
     for batch in f:
         batch = json.loads(batch)
-        requests.post(base + 'put_sessions', json=batch)
-        
+        requests.post(base + 'put_events', json=batch)
+
