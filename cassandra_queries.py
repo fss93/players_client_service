41f7043622b85ec0f6c5f1d967cbb5fe311ccec8
Sergei Fedotov <sergeyfedotov083@gmail.com>
2022-07-09 17:51:28 +0300

    Tutorial. Tested select in json format

--- cassandra_queries.py
+++ cassandra_queries.py
@@ -1,12 +1,17 @@
 from cassandra.cluster import Cluster
+import json
 
 cluster = Cluster()
 session = cluster.connect()
 session.set_keyspace('player_events')
 query = """
-SELECT * FROM end_session_events WHERE player_id = '32bb90e8976aab5298d5da10fe66f21d' LIMIT 20;
+SELECT JSON * FROM end_session_events WHERE player_id = '32bb90e8976aab5298d5da10fe66f21d' LIMIT 20;
 """
 
 t = session.execute(query)
+res = []
 for row in t:
-    print(row.player_id, row.ts)
+    res.append(json.loads(row.json))
+
+
+print(json.dumps(res))
