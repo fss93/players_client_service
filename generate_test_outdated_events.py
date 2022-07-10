48b6e204ff280a9151f8b01409da4026bbc0206f
Sergei Fedotov <sergeyfedotov083@gmail.com>
2022-07-10 20:32:06 +0300

    Added generator of testcases with outdated timestamps

--- generate_test_outdated_events.py
+++ generate_test_outdated_events.py
@@ -1,9 +1,9 @@
 """
 Generate test events and write them into file test_case_simple_upload.txt
 Create 10 countries
-Create 100 players. 10 players per country
+Create 10 players. 1 players per country
 Create 5 sessions per each player. Each session opens and closes. Total of 10 events per player
-Events' timestamps are not older than 1 month
+Events' timestamps are OLDER THAN 1 YEAR
 """
 
 import hashlib
@@ -14,42 +14,42 @@ import json
 
 countries = ['AB', 'CD', 'EF', 'GH', 'IJ', 'KL', 'MN', 'OP', 'QR', 'ST']
 
-# Create 100 players (10 per country)
+# Create 10 players (1 per country)
 players = []
-for i in range(100):
+for i in range(10):
     player_id = hashlib.md5(str(i).encode('ASCII')).hexdigest()
     players.append(player_id)
 
-# Create 500 sessions (5 per player)
+# Create 50 sessions (5 per player)
 sessions = []
-for i in range(100, 600):
+for i in range(10, 60):
     session_id = hashlib.md5(str(i).encode('ASCII')).hexdigest()
     sessions.append(session_id)
 
 
-def random_start_date():
-    start_date = datetime.datetime.now()
-    # Generate data not older than 1 month
-    rand_date = start_date - datetime.timedelta(minutes=randrange(60 * 24 * 30))
-    return rand_date.strftime("%Y-%m-%dT%H:%M:%S")
+# Start date older than 1 year
+def outdated_start_date():
+    outdated_date = datetime.datetime.now() - datetime.timedelta(days=366)
+    return outdated_date.strftime("%Y-%m-%dT%H:%M:%S")
 
 
+# End date older than year
 def random_end_date(start):
     start_date = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S")
-    end_date = start_date + datetime.timedelta(minutes=randrange(60 * 15))
+    end_date = start_date + datetime.timedelta(minutes=randrange(60))
     return end_date.strftime("%Y-%m-%dT%H:%M:%S")
 
 
 # Generate start and end events
 events = []
-for i in range(500):
-    start_dt = random_start_date()
+for i in range(50):
+    start_dt = outdated_start_date()
     end_dt = random_end_date(start_dt)
     event_start = {'session_id': sessions[i],
                    'player_id': players[i//5],
                    'ts': start_dt,
                    'event': 'start',
-                   'country': countries[i//50]
+                   'country': countries[i//5]
                    }
     event_end = {'session_id': sessions[i],
                  'player_id': players[i//5],
@@ -71,6 +71,6 @@ for event in events:
         event_batches.append(cur_batch)
         cur_batch = []
 
-with open('test_case_simple_upload.txt', 'w', encoding='utf-8') as f:
+with open('test_case_outdated_events.txt', 'w', encoding='utf-8') as f:
     for batch in event_batches:
         f.write(json.dumps(batch) + '\n')
