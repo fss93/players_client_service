d9789afed9eff36534de22989924b5cc6b921fa1
Sergei Fedotov <sergeyfedotov083@gmail.com>
2022-07-10 20:35:57 +0300

    Deleted outdated files

--- client.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-base = 'http://127.0.0.1:5000/'
-response = requests.get(base + 'helloworld')
-print(response.json())
-
-response_2 = requests.post(base + 'helloworld', json=[{'a': 1}, {'b': 2}])
-print(response_2.json())
