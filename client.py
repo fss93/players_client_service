16765cc5af64f1d04321da4024b733901d282cef
Sergei Fedotov <sergeyfedotov083@gmail.com>
2022-07-10 21:07:04 +0300

    Removed unnecessary tests from client side

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import json
 import requests
-import datetime
 
 base = 'http://127.0.0.1:5000/'
 with open('test_case_simple_upload.txt', encoding='utf-8') as f:
@@ -8,13 +7,10 @@ with open('test_case_simple_upload.txt', encoding='utf-8') as f:
         batch = json.loads(batch)
         requests.post(base + 'put_events', json=batch)
 
-# with open('test_case_ttl.txt', encoding='utf-8') as f:
-#     for batch in f:
-#         batch = json.loads(batch)
-#         requests.post(base + 'put_events', json=batch)
-
 r_end = requests.get(base + 'end_players_events/' + '32bb90e8976aab5298d5da10fe66f21d')
-print(r_end.json())
+print('-----------------End events by player-------------------')
+print(r_end.text)
 
-r_start = requests.get(base + 'start_players_events/' + '700.5')
-print(r_start.json())
+r_start = requests.get(base + 'start_players_events/' + '200.5')
+print('-----------------Start events by hours------------------')
+print(r_start.text)
